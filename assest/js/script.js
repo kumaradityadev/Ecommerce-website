b021075a0b68a79f0a6797d69bed9e35ada5f218
Aditya Kumar <adityadreamer9060@gmail.com>
2024-11-26 11:16:08 +0530

    shop page & single Product  page done

--- assest/js/script.js
+++ assest/js/script.js
@@ -12,3 +12,19 @@ if(close){
         nav.classList.remove('active')
     })
 }
+
+var MainImg = document.getElementById("mainImg")
+var smallImg = document.getElementsByClassName('small-img')
+
+smallImg[0].onclick = function(){
+    MainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    MainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    MainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+    MainImg.src = smallImg[3].src;
+}
